30ab0b2ae9d52d8fa41ca30fe36f57e4497cfab7
hhr <530266719@qq.com>
2022-05-29 15:47:07 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+<NavBar></NavBar>
+  
+  <router-view/>
+</template>
+
+<script>
+import  'bootstrap/dist/css/bootstrap.css';
+import  'bootstrap/dist/js/bootstrap';
+import  NavBar from './components/NavBar';
+
+export default{
+  name: 'App',
+  components: {
+    NavBar: NavBar,
+
+}
+}
+
+</script>
+
+
+<style>
+
+</style>
